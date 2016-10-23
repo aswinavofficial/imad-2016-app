14a1872afb599b330ad238ca11a8592e28328080
aswinavofficial <aswinavofficial@gmail.com>
2016-10-23 19:09:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
+
+app.get('/ui/styled.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'styled.css'));
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
